1d4e0a2c4306ab77344dd3659d6bef92de8e2be9
gaby1994 <g.degez@cfa-insta.fr>
2022-10-29 11:42:30 +0200

    Add files via upload

--- frontend/screens/AgendaScreen.tsx
+++ frontend/screens/AgendaScreen.tsx
@@ -1,15 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { Platform, StyleSheet } from 'react-native';
+import { Calendar, CalendarList, Agenda, LocaleConfig } from 'react-native-calendars';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootStackScreenProps, RootTabScreenProps } from '../types';
 
 export default function AgendaScreen({ navigation }: RootTabScreenProps<'Agenda'>) {
+  LocaleConfig.locales['fr'] = {
+    monthNames: [
+      'Janvier',
+      'Février',
+      'Mars',
+      'Avril',
+      'Mai',
+      'Juin',
+      'Juillet',
+      'Août',
+      'Septembre',
+      'Octobre',
+      'Novembre',
+      'Décembre'
+    ],
+    monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
+    dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+    dayNamesShort: ['Dim.', 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.']
+  };
+
+  LocaleConfig.defaultLocale = 'fr';
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Article</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.title}>Calendrier</Text>
+      <Calendar 
+        initialDate={'2022-10-01'}
+        minDate={'2022-10-01'}
+        maxDate={'2023-12-31'}
+        onDayPress={day => {
+          console.log('selected day', day);
+        }}
+        onDayLongPress={day => {
+          console.log('selected day', day);
+        }}
+        monthFormat={'yyyy MM'}
+        onMonthChange={month => {
+          console.log('month changed', month);
+        }}
+        hideExtraDays={true}
+        disableMonthChange={true}
+        firstDay={1}
+        hideDayNames={true}
+        showWeekNumbers={true}
+        onPressArrowLeft={subtractMonth => subtractMonth()}
+        onPressArrowRight={addMonth => addMonth()}
+        disableArrowLeft={true}
+        disableArrowRight={true}
+        disableAllTouchEventsForDisabledDays={true} 
+        enableSwipeMonths={true}
+        markedDates={{
+          'today': {selected: true, marked: true, selectedColor: 'blue'}
+        }}
+        />
+        <View style={styles.inputLine}>
+          <Text style={styles.labelText}>Lorem Ipsum</Text>
+        </View>
     </View>
   );
 }
@@ -29,4 +84,17 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  textInput: {
+    borderBottomWidth:2,
+    flex:1
+  },
+  inputLine: {
+    flexDirection:'row',
+    margin:10,
+    flex:1
+  },
+  labelText: {
+    margin:10,
+    flex:1
+  }
 });
--- frontend/screens/ContactScreen.tsx
+++ frontend/screens/ContactScreen.tsx
@@ -1,16 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet, AppRegistry, TextInput, TouchableHighlight } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootStackScreenProps, RootTabScreenProps } from '../types';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
 
 export default function ContactScreen({ navigation }: RootTabScreenProps<'Contact'>) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Contact</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-    </View>
+      <View style={[styles.balloon, { backgroundColor: '#1084ff' }]}>
+        <Text style={{ paddingTop: 5, color: 'white' }}>Hey!</Text>
+        <View
+          style={[
+            styles.arrowContainer,
+            styles.arrowLeftContainer,
+          ]}
+        >
+          <View style={styles.arrowLeft}>
+          </View>
+        </View>
+      </View>
+      <View style={styles.inputLine}>
+        <Text style={styles.labelText}>Saisissez votre message</Text>
+        <TextInput style={styles.textInput}
+          value=""/>
+          <TouchableHighlight style={styles.button}>
+                <Text style={styles.buttonText}>Envoyer</Text>
+            </TouchableHighlight>
+      </View>
+    </View>  
   );
 }
 
@@ -29,5 +52,67 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  item: {
+    marginVertical: 14,
+    flexDirection: 'row'
+ },
+ itemIn: {
+     marginLeft: 10
+ },
+ itemOut: {
+    alignSelf: 'flex-end',
+    marginRight: 10
+ },
+ balloon: {
+    maxWidth: scale(250),
+    paddingHorizontal: 15,
+    paddingTop: 10,
+    paddingBottom: 15,
+    borderRadius: 20,
+ },
+ arrowContainer: {
+     position: 'absolute',
+     top: 0,
+     left: 0,
+     right: 0,
+     bottom: 0,
+     zIndex: -1
+     // backgroundColor: 'red'
+ },
+ arrowLeftContainer: {
+     justifyContent: 'center',
+     alignItems: 'flex-start',
+     // backgroundColor: 'green'
+ },
+ 
+ arrowLeft: {
+     left: -20,
+ },
+ textInput: {
+  borderBottomWidth:2,
+  flex:1
+},
+inputLine: {
+  flexDirection:'row',
+  margin:10,
+  flex:1
+},
+labelText: {
+  margin:10,
+  flex:1
+},
+button:{
+  backgroundColor: Colors.light.tint,
+  padding:10,
+  marginTop:20
+},
+buttonText:{
+  color: 'white',
+  textAlign:'center'
+}
 });
+
+function scale(arg0: number): any | string | number | undefined {
+  throw new Error('Function not implemented.');
+}
 
